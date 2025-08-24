e52991ebaf4b1506076103cf8ed401b87bfdd5ea
Dev-43 <deveshdolas9@gmail.com>
2025-08-24 21:27:29 +0530

    Update 1493.cpp

--- 1493.cpp
+++ 1493.cpp
@@ -48,8 +48,6 @@ Given a binary array nums, you should delete one element from it.
 
 Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 
- 
-
 Example 1:
 
 Input: nums = [1,1,0,1]
@@ -64,4 +62,10 @@ Example 3:
 
 Input: nums = [1,1,1]
 Output: 2
-Explanation: You must delete one element.*/
+
+Explanation: You must delete one element.
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
