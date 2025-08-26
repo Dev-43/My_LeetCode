48d99e75483caad6bacdad0b10c77ec3680ad668
Dev-43 <deveshdolas9@gmail.com>
2025-08-26 22:36:06 +0530

    Update 1493.cpp

--- 1493.cpp
+++ 1493.cpp
@@ -43,7 +43,7 @@ int main(){
 
 
 /*
-Qno-1493
+Qno-1493 Longest Subarray of 1's After Deleting One Element
 Given a binary array nums, you should delete one element from it.
 
 Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
@@ -69,3 +69,4 @@ Constraints:
 1 <= nums.length <= 105
 nums[i] is either 0 or 1.
 */
+
